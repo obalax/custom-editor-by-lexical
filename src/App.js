1d9e0cfd7a4ac18842dcd5da79d9868e9cec7010
obalax <obalax@yahoo.com>
2023-04-07 07:24:32 +0200

    Default config setting

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import CustomEditor from "./components/CustomEditor";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Rich Text Example</h1>
+      <p>This is an experimental build of Lexical</p>
+      <CustomEditor />
+    </div>
+  );
+}
+
+export default App;
